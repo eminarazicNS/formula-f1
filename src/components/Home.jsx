5818cd4260bee71ebd8de26d0e68b85dcce59da1
emina.razic <emina.razic@gmail.com>
2026-06-11 21:43:46 +0200

    footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,9 @@ export default function Home(props) {
 
     return (
         <footer>
-            <p>&copy; 2026 Green classroom group II. All rights reserved.</p>
+            <p>&copy; 2026 Emina Razic.</p>
             <address>
-                Contact us at <a href="mailto:school@smartschool.rs">school@smartschool.rs</a>
+                Contact me at <a href="mailto:emina.razic@gmail.com">emina.razic@gmail.com</a>
             </address>
         </footer>
     );
