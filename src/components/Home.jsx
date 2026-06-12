d05ca2333f5e8a43f55de6efd6ec3b9358d4bf86
emina.razic <emina.razic@gmail.com>
2026-06-12 15:11:04 +0200

    New style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 export default function Home(props) {
 
-    return (
+    return ( 
         <footer>
-            <p>&copy; 2026 Emina Razic.</p>
+            <p>&copy; 2026 Emina Razic</p>
             <address>
                 Contact me at <a href="mailto:emina.razic@gmail.com">emina.razic@gmail.com</a>
             </address>
-        </footer>
+        </footer> 
     );
 }
